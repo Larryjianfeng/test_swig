3b456e6d042dfee5054fd03b44ac478ea1ce22d2
Larry Yan <stevenjobsbsbsb@hotmail.com>
2019-04-12 18:51:16 +0800

    fix

--- zmq_ex/push_pull/z_server.py
+++ zmq_ex/push_pull/z_server.py
@@ -1,12 +1,17 @@
 import zmq
 import sys
+import jieba
+
+server_id = sys.argv[1].encode()
+
+# this is the model part. can be imported outside
+def trans(contents):
+    return ' '.join(jieba.cut(contents)).encode()
 
 
-server_id = sys.argv[1]
 def main():
-    """ main method """
 
-    # Prepare our context and publisher
+    # initial subcriber part and receiver part
     context = zmq.Context()
     subscriber = context.socket(zmq.SUB)
     subscriber.connect("tcp://localhost:5563")
@@ -17,12 +22,16 @@ def main():
     receiver.connect("tcp://localhost:5565")
 
     while True:
-        # Read envelope with address
+        # Receive from proxy
         [address, _id, contents] = subscriber.recv_multipart()
-        # print("%s, %s , %s" % (address, contents.decode(), server_id))
-        receiver.send_multipart([_id, contents])
+        print('server received')
+        # process data
+        contents = trans(contents)
+        # send out data
+        receiver.send_multipart([_id, contents, server_id])
+        print('server send')
 
-    # We never get here but clean up anyhow
+    # destroy context
     subscriber.close()
     context.term()
 
